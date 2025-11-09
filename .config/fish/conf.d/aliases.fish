1b3420e1b64607d318fb1249f3c2a0aad4071e95
Ehsan Tork <journalehsan@gmail.com>
2025-11-10 02:27:17 +0330

    Update configuration files and add update-with-proxy script

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,2 +1,5 @@
 # Alias for add_wallpaper script
 alias add_wallpaper="~/.local/bin/add_wallpaper"
+
+# Alias for update with proxy script
+alias update_with_proxy="~/dotfiles/scripts/update-with-proxy"
